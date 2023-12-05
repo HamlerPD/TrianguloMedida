952c9a00d01f0723e9bbf135edc0ece620d3744a
hamle <hamlersd6@gmail.com>
2023-12-04 21:51:56 -0400

    tercer ejercio creacion de clases

---
+++ Aritmetica2/Aritmetica2/CLASS/Aritmetica.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Aritmetica2.CLASS
+{
+    public class Aritmetica
+    {
+        private int num1;
+        private int num2;
+        
+
+        public Aritmetica(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+           
+        }
+
+        public int Getsuma()
+        {
+            return num1 + num2;
+        }
+
+        public int Producto()
+        {
+            return num1 * num2;
+        }
+
+        public int Division()
+        {
+            return (num1 / num2);
+        }
+
+        public int Rest()
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Aritmetica2/Aritmetica2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Aritmetica2.CLASS;
+
+Aritmetica c;
+Console.WriteLine(" Ingrese el primer numero ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(" Ingrese el segundo numero ");
+int num2 = int.Parse(Console.ReadLine());
+
+
+c = new Aritmetica(num1, num2);
+
+Console.WriteLine(" La suma de los numeros es:" + c.Getsuma() + " EL producto de los dos ultimos numeros es: " + c.Producto() +
+    " La resta de los numeros es igual a:" + c.Rest() + " La division de los numeros es igual a: " + c.Division());
