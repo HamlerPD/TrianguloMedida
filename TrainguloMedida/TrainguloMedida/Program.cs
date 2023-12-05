7f66f38d513261ed95b6eeef22d546f8ff28fbff
hamle <hamlersd6@gmail.com>
2023-12-04 21:21:29 -0400

    Ejercicio 2 creacion de clases

---
+++ TrainguloMedida/TrainguloMedida/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TrainguloMedida.CLASS;
+
+Triangulomedidaequilatero triangulomedidaequilatero = new Triangulomedidaequilatero();
+triangulomedidaequilatero.Obtenerdatos();
