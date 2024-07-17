0dd151d34fc5d69741869ef79547648f053b381e
hamle <hamlersd6@gmail.com>
2024-07-16 21:31:59 -0400

    MATRICES Y CICLO FOREACH

---
+++ Matrices/Matrices/Cibersecurity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Matrices
+{
+    public class Cibersecurity
+    {
+        public Cibersecurity()
+        {
+
+            string[] FraudulentOrdersId = new string[8];
+
+            FraudulentOrdersId[0] = "B123";
+            FraudulentOrdersId[1] = "C234";
+            FraudulentOrdersId[2] = "A345";
+            FraudulentOrdersId[3] = "C15";
+            FraudulentOrdersId[4] = "B177";
+            FraudulentOrdersId[5] = "G3003";
+            FraudulentOrdersId[6] = "C235";
+            FraudulentOrdersId[7] = "B179";
+
+          foreach (String items in FraudulentOrdersId)
+            {
+                
+                if (items.StartsWith("B"))
+                {
+                    Console.WriteLine($"This {items} orders is fraudulent: ");
+                }
+            }
+
+         
+
+
+        }
+    } 
+} 
